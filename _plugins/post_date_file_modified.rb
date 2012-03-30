7558bfe3420fcadef82a16919a6bf11fde18b64f
Ken Robertson <ken@invalidlogic.com>
2012-03-30 10:56:02 -0700

    Tweak to file modification to not break if the file isnt committed yet

--- _plugins/post_date_file_modified.rb
+++ _plugins/post_date_file_modified.rb
@@ -8,7 +8,6 @@ end
 
 module Jekyll
   class PostDateFileModified < Generator
-
     safe true
     priority :high
 
@@ -16,9 +15,10 @@ module Jekyll
       require 'grit'
       repo = Grit::Repo.new(File.join(__FILE__, '..', '..'))
       site.posts.each do |post|
-        post.date = repo.log('master', post.full_path_to_source, :max_count => 1).first.authored_date
+        if latest_commit = repo.log('master', post.full_path_to_source, :max_count => 1).first
+          post.date = latest_commit.authored_date
+        end
       end
     end
-
   end
 end
